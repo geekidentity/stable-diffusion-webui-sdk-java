1fced04efd5741467840a0c10506aa0018e090ae
houfachao <geekidentity@163.com>
2023-05-21 22:58:06 +0800

    txt2img

--- src/test/java/tech/xxlab/sd/StableDiffusionClientTest.java
+++ src/test/java/tech/xxlab/sd/StableDiffusionClientTest.java
@@ -6,6 +6,13 @@ import tech.xxlab.sd.contant.StableDiffusionConstant;
 import tech.xxlab.sd.request.Txt2ImgRequest;
 import tech.xxlab.sd.response.Txt2ImgResponse;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class StableDiffusionClientTest {
 
     private StableDiffusionClient client;
@@ -13,16 +20,27 @@ public class StableDiffusionClientTest {
     @Before
     public void init() {
         StableDiffusionClientConfiguration configuration = new StableDiffusionClientConfiguration();
-        configuration.setHost("http://nas.xxlab.tech:7860");
+        configuration.setHost("http://localhost:7860");
         client = new StableDiffusionClient(configuration);
     }
 
     @Test
-    public void txt2ImgTest() {
+    public void txt2ImgTest() throws Exception {
         Txt2ImgRequest request = new Txt2ImgRequest();
         request.setSamplerIndex(StableDiffusionConstant.SAMPLE_DPMPP_2M_KA);
         request.setPrompt("a cat");
         Txt2ImgResponse response = client.txt2Img(request);
-        System.out.println(response.getInfo());
+
+        // 获取解码后的缓冲图像数组
+        BufferedImage[] decodedImages = response.getDecodedImages();
+
+        // 将缓冲图像保存到本地文件或输出流中
+        for (int i = 0; i < decodedImages.length; i++) {
+            File outputFile = new File("output" + i + ".png");
+            ImageIO.write(decodedImages[i], "png", outputFile);
+
+            OutputStream output = new FileOutputStream(outputFile);
+            ImageIO.write(decodedImages[i], "png", output);
+        }
     }
 }
